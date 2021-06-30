197cb62578918cce43d9d4989a65ed8f1719527a
andy-mars <andy9887776666@gmail.com>
2021-06-30 17:12:49 +0800

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,63 @@
 import React, { Children, Component, MouseEventHandler } from 'react';
-import { count } from 'console';
-import classNames from 'classnames'
-
-import logo from './logo.svg';
 import './App.css'
-// css module 
-import style from './test.module.css'
-import scssStyle from './testCSS.module.scss';
-import { EventEmitter } from 'events';
-import ReactDOM from 'react-dom' 
-import routes from './routers/index'
+
+import { createStore, combineReducers } from 'redux';
+
+const defaultState: any = 0;
+
+const countReducer = (state: any, action: any) => {
+    if (action.countType === 'ADD') {
+        return state + action.payload;
+        console.log(state)
+    } else if (action.countType === 'MINUS') {
+        return state - action.payload;
+        console.log(state)
+    }else {
+        return state;
+    }
+};
+
+const setNameReducer = (state: any, action: any) => {
+    return action.name;
+}
+
+// reducer 理论上reducer应为纯函数
+const reducer = combineReducers({
+    'COUNT': countReducer,
+    'SET_NAME': setNameReducer
+});
+
+// state
+const state: any = {
+    name: 'andy',
+    count: 5,
+};
+
+// store 通过reducer保存数据 第二个值可以设置reducer初始状态
+const store = createStore(reducer, state);
+
+// store 监听数据变化
+store.subscribe(() => {
+    // state 获取当前时刻的数据
+    const state = store.getState();
+    console.log(state);
+})
+
+// action && action creator
+function addTodo(payload: number) {
+    return {
+        type: 'ADD',
+        payload,
+    }
+}
+
+const action = addTodo(10);
+
+// dispatch 发出 action 会触发数据变更
+store.dispatch(action);
+
+
+
 
 class App extends Component {
     ref: React.RefObject<any>;
@@ -19,12 +67,10 @@ class App extends Component {
         this.ref = React.createRef();
     }
     componentDidMount() {
-        // 载入路由
-        ReactDOM.render(routes, this.ref.current);
     }
     render() {
-        return <div 
-            id="app" 
+        return <div
+            id="app"
             ref={this.ref}
         ></div>
     }
--- src/components/Home/HomeRedux.tsx
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-// 引入 reducer 及 actionCreator 
-import list from './PreviewListRedux';
-export default combineReducers({
-    list,
-});
-export * as listAction from './PreviewListRedux';
